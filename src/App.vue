a6fc32d5d177c923daa5ba6f6186c3bb5ca2b7b0
Patrick <pnelson1@ycp.edu>
2021-07-08 18:31:08 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NavigationBar/>
+  </div>
+</template>
+
+<script>
+import NavigationBar from "./components/NavigationBar";
+
+export default {
+  name: "App",
+  components: {
+    NavigationBar
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        whoa, this is a navigation bar! how neat is that?
+    </div>
+</template>
+
+<script scoped>
+</script>
