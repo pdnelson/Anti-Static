3b55550f52f2d92cd1e272432aed3acd6f5104fc
Patrick <pnelson1@ycp.edu>
2021-07-09 18:44:42 -0400

    added background and more footer styles!!!

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
     <NavigationBar/>
 
     <router-view></router-view>
+    
+    <hr/>
 
-    <footer>2021 Patrick Nelson</footer>
+    <footer>{{currentYear}} Patrick Nelson</footer>
   </v-app>
 </template>
 
@@ -13,12 +15,26 @@ import NavigationBar from "./modules/NavigationBar";
 
 export default {
   name: "App",
+
   components: {
     NavigationBar
+  },
+
+  data() {
+    return {
+      currentYear: new Date().getFullYear()
+    };
   }
 };
 </script>
 
-<style scoped>
-
+<style>
+footer {
+  text-align: center;
+	margin-bottom: 10px;
+	margin-top: 10px;
+}
+#app {
+	background-image: linear-gradient(to bottom right, #838383, #5A5A5A);
+}
 </style>
