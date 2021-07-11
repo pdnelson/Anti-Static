69cccdaaf82873c1e91ffe489a19c287f89e316b
Patrick <pnelson1@ycp.edu>
2021-07-11 18:25:19 -0400

    Project view now can show projects with one image

--- src/modules/projects/Project.vue
+++ src/modules/projects/Project.vue
@@ -2,6 +2,20 @@
     <div>
         <div v-if="projectExists">
             <p class="header">{{currentProject.name}}</p>
+            <div class="dark-blob">
+                <div v-if="moreThanOneImage">
+
+                </div>
+
+                <div v-else>
+                    <div v-if="hasOneImage">
+                        <img :src="getProjectImage()" class="proj-img"/>
+                        <br/>
+                    </div>
+                </div>
+                <div class="proj-description" ref="description">
+                </div>
+            </div>
         </div>
 
         <div v-else class="doesnt-exist">
@@ -12,16 +26,22 @@
 
 <script>
 import { Projects } from './tempdata/projects';
+import { Images } from '../../data/images';
 
 export default {
+  mounted() {
+    // We must process the description this way to allow easier use of HTML styling in the blob sections...
+    this.$refs.description.innerHTML = this.currentProject.description;
+  },
   data() {
       return {
           projects: Projects,
+          images: Images
       };
   },   
   methods: {
-    getImageFromUrl: function() {
-        return require('' + this.category.lightImage);
+    getProjectImage: function() {
+        return this.projectImages[0].image;
     },
     getIdFromAnchor: function(anchor) {
         return anchor.substring(1, anchor.length);
@@ -33,6 +53,18 @@ export default {
     },
     projectExists() {
         return this.currentProject;
+    },
+    projectImages() {
+        return this.images.filter(image =>
+            image.objectType == 'project' &&
+            image.objectId == this.currentProject.id
+        )
+    },
+    moreThanOneImage() {
+        return this.projectImages.length > 1;
+    },
+    hasOneImage() {
+        return this.projectImages.length == 1;
     }
   }
 };
@@ -44,5 +76,13 @@ export default {
         margin-top: 20px;
         margin-bottom: 10px;
         font-size: 15pt;
+    }
+
+    .proj-img {
+        margin-left: auto;
+        margin-right: auto;
+        display: block;
+        max-height: 800px;
+        width: 60%;
     }
 </style>
--- src/modules/projects/tempdata/projects.js
+++ src/modules/projects/tempdata/projects.js
@@ -99,7 +99,7 @@ export const Projects = [
     },
     {
         id: 15,
-        name: 'Oscilloscope Blackjack',
+        name: 'Oscilloscope Blackjack category 5',
         year: "2019",
         categoryId: 5,
         description: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Have you ever wanted to play Blackjack with a friend using your computer, but you couldn't, because you couldn't trust them to not peek at the computer screen? I haven't, but this program aims to solve that problem for those who have! All you need to do is grab your oscilloscope and run this program! Finally, an accessible solution for every Blackjack enthusiast!"
