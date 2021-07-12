3a83f87f16541594d1a953b688805a50f166766d
Patrick <pnelson1@ycp.edu>
2021-07-11 22:03:01 -0400

    implemented image gallery for more than one image being present

--- src/data/images.js
+++ src/data/images.js
@@ -3,16 +3,46 @@ export const Images = [
         id: 4,
         objectType: 'pagesection',
         objectId: 3,
-        image: 'https://cdn.discordapp.com/attachments/625801308854812684/860893425733468220/static_cover.png'
+        image: 'https://cdn.discordapp.com/attachments/625801308854812684/863958221185482772/oblackjack.png'
     },
     {
         id: 5,
         objectType: 'pagesection',
         objectId: 8,
-        image: 'https://cdn.discordapp.com/attachments/625801308854812684/860893425733468220/static_cover.png'
+        image: 'https://cdn.discordapp.com/attachments/625801308854812684/863958221185482772/oblackjack.png'
     },
     {
         id: 6,
+        objectType: 'pagesection',
+        objectId: 8,
+        image: 'https://cdn.discordapp.com/attachments/625801308854812684/860893425733468220/static_cover.png'
+    },
+    {
+        id: 7,
+        objectType: 'pagesection',
+        objectId: 8,
+        image: 'https://cdn.discordapp.com/attachments/625801308854812684/860893425733468220/static_cover.png'
+    },
+    {
+        id: 8,
+        objectType: 'pagesection',
+        objectId: 8,
+        image: 'https://cdn.discordapp.com/attachments/625801308854812684/860893425733468220/static_cover.png'
+    },
+    {
+        id: 9,
+        objectType: 'pagesection',
+        objectId: 8,
+        image: 'https://cdn.discordapp.com/attachments/625801308854812684/860893425733468220/static_cover.png'
+    },
+    {
+        id: 10,
+        objectType: 'pagesection',
+        objectId: 8,
+        image: 'https://cdn.discordapp.com/attachments/625801308854812684/860893425733468220/static_cover.png'
+    },
+    {
+        id: 11,
         objectType: 'pagesection',
         objectId: 8,
         image: 'https://cdn.discordapp.com/attachments/625801308854812684/860893425733468220/static_cover.png'
--- src/modules/pagesection/PageSection.vue
+++ src/modules/pagesection/PageSection.vue
@@ -5,8 +5,9 @@
                 <div class="center"><h2 class="dark-item">{{pageSection.heading}}</h2></div> 
             </div>
 
-            <div v-if="moreThanOneImage">
-
+            <div v-if="moreThanOneImage" class="image-gallery">
+                <img class="image" v-for="(image, i) in pageSectionImages" :src="image" :key="i" @click="index = i">
+                <vue-gallery-slideshow :images="pageSectionImages" :index="index" @close="index=null"></vue-gallery-slideshow>
             </div>
 
             <div v-else>
@@ -23,8 +24,12 @@
 
 <script>
 import { Images } from '../../data/images';
+import VueGallerySlideshow from 'vue-gallery-slideshow';
 
 export default {
+  components: {
+    vueGallerySlideshow: VueGallerySlideshow,
+  },
   mounted() {
     // We must process the description this way to allow easier use of HTML styling in the blob sections...
     this.$refs.description.innerHTML = this.pageSection.description;
@@ -36,12 +41,13 @@ export default {
   },
   data() {
       return {
-          images: Images
+          images: Images,
+          index: null
       };
   },   
   methods: {
     getSectionImage: function() {
-        return this.pageSectionImages[0].image;
+        return this.pageSectionImages[0];
     }
   },
   computed: {
@@ -50,6 +56,7 @@ export default {
             image.objectType == 'pagesection' &&
             image.objectId == this.pageSection.id
         )
+        .map(image => image.image)
     },
     moreThanOneImage() {
         return this.pageSectionImages.length > 1;
@@ -71,5 +78,27 @@ export default {
         display: block;
         max-height: 700px;
         width: 60%;
+        box-shadow: 3px 3px 15px 3px black;
     }
+
+    .image-gallery {
+        text-align: center;
+    }
+
+    .image {
+        width: 100px;
+        height: 100px;
+        background-size: cover;
+        cursor: pointer;
+        margin: 5px;
+        border-radius: 3px;
+        border: 1px solid lightgray;
+        object-fit: contain;
+    }
+
+    @media screen and (max-width: 620px) {
+    .section-img {
+      width: 100%;
+    }
+  }
 </style>
