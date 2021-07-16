f0df6d77975d992cda4f159979e1dfe00254677e
Patrick <pnelson1@ycp.edu>
2021-07-15 22:07:13 -0400

    Current song collection now shows the name

--- src/modules/music/SongCollection.vue
+++ src/modules/music/SongCollection.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        song colleciton!!!
+        {{currentCollection.name}}
 
         <back-to-previous-page></back-to-previous-page>
 
@@ -8,11 +8,27 @@
 </template>
 
 <script>
+import { SongCollections } from './tempdata/songCollections';
+import { Songs } from './tempdata/songs';
 import BackToPreviousPage from "../utilities/BackToPreviousPage";
 
-export default {  
+export default { 
+  mounted() {
+    document.title = "Anti-Static - " + this.currentCollection.name;
+  },
   components: {
     backToPreviousPage: BackToPreviousPage
+  },
+  data() {
+      return {
+          songs: Songs,
+          songCollections: SongCollections
+      };
+  }, 
+  computed: {
+    currentCollection() {
+        return this.songCollections.find(collection => collection.id == this.$route.params.id);
+    }
   }
 };
 </script>
